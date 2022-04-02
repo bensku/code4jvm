f95aa7fa23de4f180761f971757a9a57808be0a1
bensku <bmi@iki.fi>
2022-04-03 01:09:01 +0300

    Add rest of natively supported arithmetic operations

--- src/main/java/fi/benjami/code4jvm/statement/Arithmetic.java
+++ src/main/java/fi/benjami/code4jvm/statement/Arithmetic.java
@@ -3,11 +3,17 @@ package fi.benjami.code4jvm.statement;
 import java.util.List;
 
 import fi.benjami.code4jvm.Expression;
+import fi.benjami.code4jvm.Type;
 import fi.benjami.code4jvm.Value;
 import fi.benjami.code4jvm.util.TypeCheck;
 
 import static org.objectweb.asm.Opcodes.*;
 
+/**
+ * Simple arithmetic operations that are built-in to Java language and JVM
+ * bytecode. For operations handled by {@link java.lang.Math}, call the methods
+ * yourself with {@link Type#callStatic(Type, String, Value...)}.
+ */
 public class Arithmetic {
 	
 	public static Expression add(Value lhs, Value rhs) {
@@ -16,6 +22,46 @@ public class Arithmetic {
 		return block -> {
 			return block.add(Bytecode.run(type, List.of(lhs, rhs), mv -> {
 				mv.visitInsn(type.getOpcode(IADD));
+			})).value();
+		};
+	}
+	
+	public static Expression subtract(Value lhs, Value rhs) {
+		TypeCheck.mustEqual(lhs, rhs);
+		var type = lhs.type();
+		return block -> {
+			return block.add(Bytecode.run(type, List.of(lhs, rhs), mv -> {
+				mv.visitInsn(type.getOpcode(ISUB));
+			})).value();
+		};
+	}
+	
+	public static Expression multiply(Value lhs, Value rhs) {
+		TypeCheck.mustEqual(lhs, rhs);
+		var type = lhs.type();
+		return block -> {
+			return block.add(Bytecode.run(type, List.of(lhs, rhs), mv -> {
+				mv.visitInsn(type.getOpcode(IMUL));
+			})).value();
+		};
+	}
+	
+	public static Expression divide(Value lhs, Value rhs) {
+		TypeCheck.mustEqual(lhs, rhs);
+		var type = lhs.type();
+		return block -> {
+			return block.add(Bytecode.run(type, List.of(lhs, rhs), mv -> {
+				mv.visitInsn(type.getOpcode(IDIV));
+			})).value();
+		};
+	}
+	
+	public static Expression remainder(Value lhs, Value rhs) {
+		TypeCheck.mustEqual(lhs, rhs);
+		var type = lhs.type();
+		return block -> {
+			return block.add(Bytecode.run(type, List.of(lhs, rhs), mv -> {
+				mv.visitInsn(type.getOpcode(ISUB));
 			})).value();
 		};
 	}
--- src/test/java/fi/benjami/code4jvm/test/ArithmeticTest.java
+++ src/test/java/fi/benjami/code4jvm/test/ArithmeticTest.java
@@ -3,12 +3,14 @@ package fi.benjami.code4jvm.test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
+import java.util.function.BiFunction;
 
 import org.junit.jupiter.api.Test;
 
 import fi.benjami.code4jvm.ClassDef;
+import fi.benjami.code4jvm.Expression;
 import fi.benjami.code4jvm.Type;
+import fi.benjami.code4jvm.Value;
 import fi.benjami.code4jvm.flag.Access;
 import fi.benjami.code4jvm.statement.Arithmetic;
 import fi.benjami.code4jvm.statement.Return;
@@ -17,20 +19,106 @@ public class ArithmeticTest {
 
 	private static final MethodHandles.Lookup LOOKUP = MethodHandles.lookup();
 
+	// TODO parameterized test for all number types
+	
+	private void makeTester(ClassDef def, Type type, String name, BiFunction<Value, Value, Expression> op) {
+		var method = def.addMethod(type, name, Access.PUBLIC);
+		var a = method.arg(type);
+		var b = method.arg(type);
+		var sum = method.add(op.apply(a, b)).value();
+		method.add(Return.value(sum));
+	}
+	
+	private void makeTesters(ClassDef def, BiFunction<Value, Value, Expression> op) {
+		makeTester(def, Type.BYTE, "testByte", op);
+		makeTester(def, Type.SHORT, "testShort", op);
+		makeTester(def, Type.CHAR, "testChar", op);
+		makeTester(def, Type.INT, "testInt", op);
+		makeTester(def, Type.LONG, "testLong", op);
+		makeTester(def, Type.FLOAT, "testFloat", op);
+		makeTester(def, Type.DOUBLE, "testDouble", op);
+	}
+	
+	interface Tester {
+		byte testByte(byte a, byte b);
+		short testShort(short a, short b);
+		char testChar(char a, char b);
+		int testInt(int a, int b);
+		long testLong(long a, long b);
+		float testFloat(float a, float b);
+		double testDouble(double a, double b);
+	}
+	
 	@Test
-	public void addition() throws Throwable {
+	public void add() throws Throwable {
 		var def = ClassDef.create("fi.benjami.code4jvm.test.AddNumbers", Access.PUBLIC);
+		def.interfaces(Type.of(Tester.class));
 		def.addEmptyConstructor(Access.PUBLIC);
 		
-		var method = def.addMethod(Type.INT, "sum", Access.PUBLIC);
-		var a = method.arg(Type.INT);
-		var b = method.arg(Type.INT);
-		var sum = method.add(Arithmetic.add(a, b)).value();
-		method.add(Return.value(sum));
+		makeTesters(def, Arithmetic::add);
 		
 		var lookup = LOOKUP.defineHiddenClass(def.compile(), true);
-		assertEquals(10, lookup.findVirtual(lookup.lookupClass(),
-				"sum", MethodType.methodType(int.class, int.class, int.class))
-				.invoke(TestUtils.newInstance(lookup), 5, 5));
+		var instance = (Tester) TestUtils.newInstance(lookup);
+		assertEquals(10, instance.testByte((byte) 5, (byte) 5));
+		assertEquals(-96, instance.testByte((byte) 80, (byte) 80));
+		assertEquals(50, instance.testShort((short) -50, (short) 100));
+		assertEquals(84, instance.testChar((char) 42, (char) 42));
+		assertEquals(12345, instance.testInt(12300, 45));
+		assertEquals(9.1f, instance.testFloat(4.5f, 4.6f));
+		assertEquals(123.45, instance.testDouble(123, 0.45));
+	}
+	
+	// TODO explicitly test all data types of all operations
+	
+	@Test
+	public void subtract() throws Throwable {
+		var def = ClassDef.create("fi.benjami.code4jvm.test.SubtractNumbers", Access.PUBLIC);
+		def.interfaces(Type.of(Tester.class));
+		def.addEmptyConstructor(Access.PUBLIC);
+		
+		makeTesters(def, Arithmetic::subtract);
+		
+		var lookup = LOOKUP.defineHiddenClass(def.compile(), true);
+		var instance = (Tester) TestUtils.newInstance(lookup);
+		assertEquals(-5, instance.testInt(5, 10));
+	}
+	
+	@Test
+	public void multiply() throws Throwable {
+		var def = ClassDef.create("fi.benjami.code4jvm.test.MultiplyNumbers", Access.PUBLIC);
+		def.interfaces(Type.of(Tester.class));
+		def.addEmptyConstructor(Access.PUBLIC);
+		
+		makeTesters(def, Arithmetic::multiply);
+		
+		var lookup = LOOKUP.defineHiddenClass(def.compile(), true);
+		var instance = (Tester) TestUtils.newInstance(lookup);
+		assertEquals(9, instance.testInt(3, 3));
+	}
+	
+	@Test
+	public void divide() throws Throwable {
+		var def = ClassDef.create("fi.benjami.code4jvm.test.DivideNumbers", Access.PUBLIC);
+		def.interfaces(Type.of(Tester.class));
+		def.addEmptyConstructor(Access.PUBLIC);
+		
+		makeTesters(def, Arithmetic::divide);
+		
+		var lookup = LOOKUP.defineHiddenClass(def.compile(), true);
+		var instance = (Tester) TestUtils.newInstance(lookup);
+		assertEquals(24, instance.testInt(120, 5));
+	}
+	
+	@Test
+	public void remainder() throws Throwable {
+		var def = ClassDef.create("fi.benjami.code4jvm.test.RemainderOfNumbers", Access.PUBLIC);
+		def.interfaces(Type.of(Tester.class));
+		def.addEmptyConstructor(Access.PUBLIC);
+		
+		makeTesters(def, Arithmetic::remainder);
+		
+		var lookup = LOOKUP.defineHiddenClass(def.compile(), true);
+		var instance = (Tester) TestUtils.newInstance(lookup);
+		assertEquals(4, instance.testInt(10, 6));
 	}
 }
