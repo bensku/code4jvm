6c8afc218fc14fe05287710ce281064a36f96021
bensku <bmi@iki.fi>
2023-01-20 17:04:46 +0200

    Add support for Java 7-16 bytecode
    Note that old JVMs are not tested (nor would it be easy to do that). This
    means that executing code on old JVMs may or may not work.

--- src/main/java/fi/benjami/code4jvm/config/JavaVersion.java
+++ src/main/java/fi/benjami/code4jvm/config/JavaVersion.java
@@ -8,6 +8,17 @@ import org.objectweb.asm.Opcodes;
  */
 public enum JavaVersion {
 
+	// FIXME although lambdas can be compiled for Java 7 bytecode, LambdaMetafactory is not available
+	JAVA_7(Opcodes.V1_7),
+	JAVA_8(Opcodes.V1_8),
+	JAVA_9(Opcodes.V9),
+	JAVA_10(Opcodes.V10),
+	JAVA_11(Opcodes.V11),
+	JAVA_12(Opcodes.V12),
+	JAVA_13(Opcodes.V13),
+	JAVA_14(Opcodes.V14),
+	JAVA_15(Opcodes.V15),
+	JAVA_16(Opcodes.V16),
 	JAVA_17(Opcodes.V17);
 	
 	private final int opcode;
